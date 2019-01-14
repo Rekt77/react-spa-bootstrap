97ceb62969a97a8c36e4555f0d4c34972bc2c5f4
Rekt77 <timtaeil@gmail.com>
2019-01-14 20:24:09 +0900

    modify component : Menu.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import Menu from 'components/Menu'
 import {
     Home,
-    About
+    About,
+    Posts
 } from 'pages';
 
 const App = () => {
     return(
       <div>
+        <Menu/>
         <Route exact path="/" component={Home} />
         <Route path="/about/:name?" component={About} />
+        <Route path="/posts" component={Posts} />
       </div>
     )
 }
